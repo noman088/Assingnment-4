94e156766b3e8ec55deb98d6fb8ee677fbc2dbdb
Md Noman <109752092+noman088@users.noreply.github.com>
2022-10-03 23:44:58 +0530

    Add files via upload

---
+++ JRB4.c
@@ -0,0 +1,131 @@
+// 1. Write a program to print MySirG 5 times on the screen
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i = 1;
+//     while (i<=5)
+
+//     {
+//         printf(" %d.MySirG \n",i);
+//         i++;
+//         /* code */
+// }
+
+// return 0 ;
+// }
+
+// 2. Write a program to print the first 10 natural numbers.
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i;
+//     for ( i = 1; i <=10; i++)
+//     {
+//     printf("%d ", i);
+//         /* code */
+//     }
+//     return 0;
+// }
+
+// 3. Write a program to print the first 10 natural numbers in reverse order
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i;
+//     for (i = 1; i <= 10;i++)
+//         printf("%d ", 11 - i);
+//     return 0;
+// }
+// 4. Write a program to print the first 10 odd natural numbers
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i;
+//     for (i = 1; i <= 10;i++)
+//         printf("%d ", 2 * i - 1);
+//     return 0;
+// }
+// 5. Write a program to print the first 10 odd natural numbers in reverse order.
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i = 10;
+//     for ( i = 10; i >=1; i--)
+//     {
+//         printf("%d ", 2*i-1);
+
+//     }
+//     return 0;
+// }
+// 6. Write a program to print the first 10 even natural numbers
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i;
+//     for (i = 1; i <= 10;i++)
+//         printf("%d ", 2 * i);
+
+//     return 0;
+// }
+// 7. Write a program to print the first 10 even natural numbers in reverse order
+
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i;
+//     for ( i = 10; i >=1 ; i--)
+//     {
+//         printf("%d ", 2 * i);     
+//     }
+//     return 0;
+// }
+// 8. Write a program to print squares of the first 10 natural numbers
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i=1;
+//     while (i<=10)
+//     {
+//         printf("%d ", i * i);
+//         i++;
+//     }
+    
+//     return 0;
+// }
+// 9. Write a program to print cubes of the first 10 natural numbers
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+// {
+//     int i = 1;
+//     while (i<=10)
+//     {
+//         printf("%d ", i * i * i);
+//         i++;
+//         /* code */
+//     }
+    
+//     return 0;
+// }
+// 10. Write a program to print a table of 5.
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
+
+// {
+//     int i, a = 5;
+//     for (i = 1; i <= 10;i++)
+// {
+//         printf("%d, ",i*a);
+// }
+
+//     return 0;
+// }
